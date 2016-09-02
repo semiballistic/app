0a7ecce239f09bf399b6b8e3232788604bf145be
threex3 <pius@outlook.com>
2016-09-02 18:20:30 -0400

    Signed-off-by: threex3 <pius@outlook.com>

--- indico.php
+++ indico.php
@@ -5,10 +5,10 @@ IndicoIo::$config['api_key'] = '404dfbb6e80ebc11c8f57a0404463c10';
 
 $s = array();
 
-$s = IndicoIo::emotion([
-    "I did it. I got into Grad School. Not just any program, but a GREAT program. :-)",
-    "Like seriously my life is bleak, I have been unemployed for almost a year."
-]);
+#$s = IndicoIo::emotion([
+#    "I did it. I got into Grad School. Not just any program, but a GREAT program. :-)",
+#    "Like seriously my life is bleak, I have been unemployed for almost a year."
+#]);
 
 var_dump($s);
 ?>
