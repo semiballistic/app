cfc6d269e6ad6cee66f0d97e61a930ad3f203e81
threex3 <pius@outlook.com>
2016-09-02 17:50:08 -0400

    2

--- index.php
+++ index.php
@@ -1,16 +1,3 @@
 <?php
-
-require(__DIR__ . '/vendor/autoload.php');
-use \IndicoIo\IndicoIo as IndicoIo;
-IndicoIo::$config['api_key'] = '404dfbb6e80ebc11c8f57a0404463c10';
-
-$s = array();
-# batch example
-/*
-$s = IndicoIo::emotion([
-    "I love writing code!",
-    "Alexander and the Terrible, Horrible, No Good, Very Bad Day"
-]);
-*/
-var_dump($s);
+echo "hello";
 ?>
---
+++ indico.php
@@ -0,0 +1,18 @@
+<?php
+
+require(__DIR__ . '/vendor/autoload.php');
+use \IndicoIo\IndicoIo as IndicoIo;
+IndicoIo::$config['api_key'] = '404dfbb6e80ebc11c8f57a0404463c10';
+
+# single example
+IndicoIo::emotion(
+    "I did it. I got into Grad School. Not just any program, but a GREAT program. :-)"
+);
+
+# batch example
+IndicoIo::emotion([
+    "I did it. I got into Grad School. Not just any program, but a GREAT program. :-)",
+    "Like seriously my life is bleak, I have been unemployed for almost a year."
+]);
+
+?>
