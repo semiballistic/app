6220c822d3a350d6bfb42b9707ae2d1141782dea
threex3 <pius@outlook.com>
2016-09-02 21:58:57 -0400

    Change-Id: I9ba5b5d38a679f04bc1bda70f49a495a3787aec1

--- indico.php
+++ indico.php
@@ -2,7 +2,7 @@
 
 require(__DIR__ . '/vendor/autoload.php');
 use \IndicoIo\IndicoIo as IndicoIo;
-IndicoIo::$config['api_key'] = '1';
+IndicoIo::$config['api_key'] = '47ba010731895352db83e7ce50a62d3d';
 
 $s = array ();
 
