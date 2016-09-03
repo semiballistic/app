ff0945aea64cac80cf0499c4572268c746e57cc6
threex3 <pius@outlook.com>
2016-09-03 17:59:14 -0400

    Change-Id: I5232cf62122a11b6b9b94ea40b1a60e2027f3408 Signed-off-by: threex3 <pius@outlook.com>

--- indico.php
+++ indico.php
@@ -1,8 +1,16 @@
 <?php
+
 require(__DIR__ . '/vendor/autoload.php');
 use \IndicoIo\IndicoIo as IndicoIo;
-IndicoIo::$config['api_key'] = '47ba010731895352db83e7ce50a62d3d';
 
-# single example
-var_dump(IndicoIo::emotion("I"));
+IndicoIo::sentiment(
+    ['indico is so easy to use!', 'Still really easy, yiss'],
+    $api_key="47ba010731895352db83e7ce50a62d3d"
+);
+
+// Array ( 
+//	  [0] => 0.9782025594088044,
+//	  [1] => 0.9895808115135271
+// )
+
 ?>
