4955f6f4cd01b90250ec680752ce895909354d4e
threex3 <pius@outlook.com>
2016-09-03 15:17:47 -0400

    Change-Id: I465ceb294246cbe1013c348205958b1ae9f51ed7

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 require('vendor/autoload.php');
 use \IndicoIo\IndicoIo as IndicoIo;
 IndicoIo::$config['api_key'] = '47ba010731895352db83e7ce50a62d3d';
-echo __DIR__;
-#echo IndicoIo::emotion(
-#    "I did it. I got into Grad School. Not just any program, but a GREAT program. :-)"
-#    );
+
+echo IndicoIo::emotion(
+    "I did it. I got into Grad School. Not just any program, but a GREAT program. :-)"
+    );
 ?>
