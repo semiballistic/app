490c9a5fe1c5aaa316749231cfe9caf243e9d2fe
threex3 <pius@outlook.com>
2016-09-03 17:31:51 -0400

    Change-Id: Ic432da972ce39f5340753010e9c34f253edd0188 Signed-off-by: threex3 <pius@outlook.com>

--- indico.php
+++ indico.php
@@ -4,7 +4,5 @@ use \IndicoIo\IndicoIo as IndicoIo;
 IndicoIo::$config['api_key'] = '47ba010731895352db83e7ce50a62d3d';
 
 # single example
-IndicoIo::emotion(
-"I did it. I got into Grad School. Not just any program, but a GREAT program. :-)"
-);
+var_dump(IndicoIo::emotion("I"));
 ?>
