7ada10211b42b28fd402bece2fe8ad219952e651
threex3 <pius@outlook.com>
2016-09-03 17:24:34 -0400

    Change-Id: Id7dfc551f339aa25a3e8895529ebfdbbf6e1cc11 Signed-off-by: threex3 <pius@outlook.com>

--- indico.php
+++ indico.php
@@ -1,5 +1,5 @@
 <?php
-require('/vendor/autoload.php');
+require(__DIR__ . '/vendor/autoload.php');
 use \IndicoIo\IndicoIo as IndicoIo;
 IndicoIo::$config['api_key'] = '47ba010731895352db83e7ce50a62d3d';
 
