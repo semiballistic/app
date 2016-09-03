e22bf306083bd2bcfd3a6027e354f33f1b8c03bc
threex3 <pius@outlook.com>
2016-09-02 20:08:04 -0400

    Change-Id: Ic5d8662549145abbf02a0787cbf1f363753ef720

--- indico.php
+++ indico.php
@@ -4,15 +4,13 @@ require(__DIR__ . '/vendor/autoload.php');
 use \IndicoIo\IndicoIo as IndicoIo;
 IndicoIo::$config['api_key'] = '47ba010731895352db83e7ce50a62d3d';
 
-# single example
-IndicoIo::sentiment(
-    "I love writing code!"
-);
+$s = array ();
 
 # batch example
-IndicoIo::sentiment([
+$s = IndicoIo::sentiment([
     "I love writing code!",
     "Alexander and the Terrible, Horrible, No Good, Very Bad Day"
 ]);
 
+var_dump($s);
 ?>
