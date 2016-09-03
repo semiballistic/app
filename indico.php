a684d6ee370f894c1f2d377a16b2e51aa63cf8f1
threex3 <pius@outlook.com>
2016-09-02 20:20:02 -0400

    Change-Id: Ie0c28ff7db2fa5be48dc86205d30d642bde00ac2

--- indico.php
+++ indico.php
@@ -7,10 +7,10 @@ IndicoIo::$config['api_key'] = '47ba010731895352db83e7ce50a62d3d';
 $s = array ();
 
 # batch example
-#$s = IndicoIo::sentiment([
-#    "I love writing code!",
-#    "Alexander and the Terrible, Horrible, No Good, Very Bad Day"
-#]);
+$s = IndicoIo::sentiment([
+    "I love writing code!",
+    "Alexander and the Terrible, Horrible, No Good, Very Bad Day"
+]);
 
 var_dump($s);
 ?>
