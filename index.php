ab52798d53f3dd8a1a787096d16f23bee4ecb72c
threex3 <pius@outlook.com>
2016-09-04 21:12:55 -0400

    Change-Id: Ie584e99902839afc76b1ec4b977f36824e91b98d Signed-off-by: threex3 <pius@outlook.com>

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php
-echo "hello world!";
+echo '__DIR__                            : ', __DIR__ , "\n";
+echo '$_SERVER["DOCUMENT_ROOT"]          : ', $_SERVER["DOCUMENT_ROOT"] , "\n";
+echo 'realpath($_SERVER["DOCUMENT_ROOT"]): ', realpath($_SERVER["DOCUMENT_ROOT"]), "\n";
 ?>
