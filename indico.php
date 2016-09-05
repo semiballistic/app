7202c1d0a1b24c2d6df27b23ffa505bdc60c8c32
threex3 <pius@outlook.com>
2016-09-04 21:11:19 -0400

    Change-Id: Ic7bfff67ac10fb7aae57178a79e25d9f375c7765 Signed-off-by: threex3 <pius@outlook.com>

--- indico.php
+++ indico.php
@@ -1,6 +1,6 @@
 <?php
 
-require(__DIR__ . '/vendor/autoload.php');
+require("http://icepick.azurewebsites.net" . '/vendor/autoload.php');
 use \IndicoIo\IndicoIo as IndicoIo;
 
 IndicoIo::sentiment(
