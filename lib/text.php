7b07ac337ca8f740bba438cc1cee5270c5f2e087
threex3 <pius@outlook.com>
2016-09-15 17:26:40 -0400

    Change-Id: I21b343ddb7bc2c11e2483b2e2c4ded4aec3822e7 Signed-off-by: threex3 <pius@outlook.com>

--- lib/text.php
+++
@@ -1,8 +0,0 @@
-<?php
-function remove_bracketed($string) {
-    $string = preg_replace("/\([^)]+\)/", "", $string);
-    $string = preg_replace("/\[([^\[\]]++|(?R))*+\]/", "", $string);
-    return $string;
-}
-
-?>
--- views/csv.php
+++
@@ -1,4 +0,0 @@
-<?php
-require ("../app.php");
-dump_csv();
-?>
